c1c8860b101c65505f28ec9df774b01baeda7d2e
dnmacedo <dnmacedo@ucdavis.edu>
2017-12-11 20:39:43 -0800

    javascript

---
+++ script.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function(event){
+  console.log("DOM fully loaded and parsed");
+
+  // artlink events
+
+  var jMrazLink = document.getElementById("jMrazLink");
+  var threeWomenLink = document.getElementById("threeWomenLink");
+  var beachLink = document.getElementById("beachLink");
+  var fmaLink = document.getElementById("fmaLink");
+  var foxLink = document.getElementById("foxLink");
+  var leoLink = document.getElementById("leoLink");
+  var roxasLink = document.getElementById("roxasLink");
+  var roxLink = document.getElementById("roxLink");
+  var spiderMLink = document.getElementById("spiderMLink");
+  var stillLifeLink = document.getElementById("stillLifeLink");
+  var venLink = document.getElementById("venLink");
+
+  var artLink = document.getElementsByClassName("art");
+  var previewImg = document.getElementById("previewImg");
+  var artTitle = document.getElementById("artTitle");
+  var artDes = document.getElementById("artDes");
+
+  // artlink events
+
+  // photo class
+
+  // class Photo {
+  //
+  // }
+
+  // photo class
+
+  // artlink function
+
+  var fileName;
+  var title;
+  var description;
+
+  function preview(fileName, title, description){
+
+      previewImg.src = "images/artwork/" + fileName;
+      artTitle.innerHTML = title;
+      artDes.innerHTML = description;
+
+  }
+
+  jMrazLink.addEventListener("clicked", preview("jMraz.jpg", "Portrait #1", "Portrait of singer/songwriter Jason Mraz, charcoal on bristol paper, 2012"));
+  threeWomenLink.addEventListener("clicked", preview("3women.jpg", "3 Women", "Colored pencil on tissue paper, 2011"));
+  beachLink.addEventListener("clicked", preview("beach.jpg", "The Beach", "Watercolor, watercolor paper, 2011"));
+
+
+  // artlink function
+
+  // photolink events
+
+
+
+  // end of photolink events
+
+});
