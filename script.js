87ed6cec89d2726a7c523e5802ace28dba9b7371
dnmacedo <dnmacedo@ucdavis.edu>
2017-12-14 23:08:13 -0800

    contact form

--- script.js
+++ script.js
@@ -57,4 +57,44 @@ document.addEventListener("DOMContentLoaded", function(event){
 
   // end of photolink events
 
+  // Contact emailForm
+
+  // capture name entry
+
+  var entry = [];
+
+// capture submit event
+  document.emailForm.onsubmit = submitForm;
+
+  function submitForm() {
+
+    entry[0]= document.emailForm.name.value;
+
+    entry[1]= document.emailForm.name.value;
+
+    //store myMsg in a variable called myMsg
+
+    var myMsg = document.getElementById("myMsg");
+
+    //error detection
+
+    for(var i=0; i<10; i++) {
+      if(entry[0]=="") {
+        alert("Let's not be strangers, please tell me your name!");
+      } else if (entry[2] =="") {
+        alert("Don't leave me hanging, leave a message!")
+      }
+    }
+
+    myMsg.innerHTML="Thank you" + entry[0] +" for visiting my site and reaching out. I have receieved your message via email and you'll be hearing back from me fairly soon via the contact info you provided. " + entry[1];
+
+    return false;
+  }
+
+  close.addEventListener('click', function(){
+    aside.style.display="none";
+  });
+
+  // contact emailForm
+
 });
