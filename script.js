1279e1bb6062ddd3ed86aedc192c7398643addf6
dnmacedo <dnmacedo@ucdavis.edu>
2017-12-17 15:08:54 -0800

    adding js to nav

--- script.js
+++ script.js
@@ -20,6 +20,31 @@ document.addEventListener("DOMContentLoaded", function(event){
   var artTitle = document.getElementById("artTitle");
   var artDes = document.getElementById("artDes");
 
+
+
+  // nav link ids
+  var homeNav = document.getElementById("homeNav");
+  var artNav = document.getElementById("artNav");
+  var graphicNav = document.getElementById("graphicNav");
+  var photoNav = document.getElementById("photoNav");
+  var contactNav = document.getElementById("contactNav");
+
+  var navLink = ['homeNav', 'artNav', 'graphicNav', 'photoNav', 'contactNav'];
+  // nav link ids
+
+  // when link is selected, make it appear different than other nav links
+
+    for(var = i; i<5; i++){
+
+      navLink[i].addEventListener('click',
+        function(){
+          this.style.display = "background-color:pink";
+      });
+
+    }
+
+
+
   // artlink events
 
   // photo class
